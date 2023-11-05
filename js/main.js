2188334bd6fdfb4a94c4114fa5e7450c231f1e67
Jaeeun <jaeeun9862@gmail.com>
2023-11-05 19:59:32 +0900

    API 데이터 변경으로 인한 수정 및 주석 추가

--- js/main.js
+++ js/main.js
@@ -1,33 +1,35 @@
 const covid_KEY = config.covid_KEY;
 
 // 전체 확진 현황
+// https://api.corona-19.kr
+// https://github.com/dhlife09/Corona-19-API?utm_source=keygen-email
 fetch(`https://api.corona-19.kr/korea/?serviceKey=${covid_KEY}`)
   .then((response) => response.json())
   .then((data) => {
-    const updateTime = data.updateTime.slice(23, -1); //업데이트시간
-    const TotalCase = data.TotalCase; // 전체 확진자수
-    const TotalDeath = data.TotalDeath; // 전체 사망자수
+    const updateTime = data.API.updateTime.slice(14, -1); // 전체 현황 업데이트시간
+    const TotalCase = data.korea.totalCnt; // 전체 확진자수
+    const TotalDeath = data.korea.deathCnt; // 전체 사망자수
 
     document.querySelector(
-      ".total_update_time"
+      '.total_update_time'
     ).innerText = `전체 현황 : ${updateTime}`;
-    document.querySelector(".total_case").innerText = TotalCase;
-    document.querySelector(".total_death").innerText = TotalDeath;
+    document.querySelector('.total_case').innerText = TotalCase;
+    document.querySelector('.total_death').innerText = TotalDeath;
   })
   .catch((error) => alert(error));
 
 // 예방접종 현황
+// https://api.corona-19.kr
+// https://github.com/dhlife09/Corona-19-API?utm_source=keygen-email
 fetch(`https://api.corona-19.kr/korea/vaccine/?serviceKey=${covid_KEY}`)
   .then((response) => response.json())
   .then((data) => {
     const vacList = [
       data.korea.vaccine_1.vaccine_1, //1차 접종 완료자
       data.korea.vaccine_2.vaccine_2, //2차 접종 완료자
-      data.korea.vaccine_3.vaccine_3, //3차 접종 완료자
-      data.korea.vaccine_4.vaccine_4, //4차 접종 완료자
     ];
 
-    const vac = document.querySelectorAll(".vac");
+    const vac = document.querySelectorAll('.vac');
 
     vac.forEach((item, idx) => {
       vacList[idx] = vacList[idx].toLocaleString();
@@ -39,6 +41,8 @@ fetch(`https://api.corona-19.kr/korea/vaccine/?serviceKey=${covid_KEY}`)
   .catch((error) => console.log(error));
 
 // 일일 확진 현황 (공공데이터)
+// 질병관리청_코로나19 국내발생현황 조회
+// https://www.data.go.kr/index.do
 const covid_today_KEY = config.covid_today_KEY;
 
 fetch(
@@ -46,24 +50,24 @@ fetch(
 )
   .then((response) => response.json())
   .then((data) => {
-    let TodayInfo = data["response"]["result"][0];
-
+    let TodayInfo = data['response']['result'][0];
     document.querySelector(
-      ".today_update_time"
+      '.today_update_time'
     ).innerText = `일일 현황 : ${TodayInfo.mmddhh} 기준`;
     document.querySelector(
-      ".today_case"
+      '.today_case'
     ).innerText = `+ ${TodayInfo.cnt_confirmations}`;
     document.querySelector(
-      ".today_death"
+      '.today_death'
     ).innerText = `+ ${TodayInfo.cnt_deaths}`;
-    document.querySelector(
-      ".today_hospital"
-    ).innerText = ` + ${TodayInfo.cnt_hospitalizations}`;
+    document.querySelector('.today_hospital').innerText =
+      TodayInfo.cnt_hospitalizations !== undefined
+        ? `-`
+        : ` + ${TodayInfo.cnt_hospitalizations}`;
   })
   .catch((error) => console.log(error));
 
 // 팝업으로 지도 띄우기
 function popup() {
-  window.open("map.html", "map_popup", "width=700, height=600");
+  window.open('map.html', 'map_popup', 'width=700, height=600');
 }
