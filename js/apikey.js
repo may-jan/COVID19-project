de638a21921b4de643b38c545ba0373343207db9
Jaeeun <jaeeun9862@gmail.com>
2023-11-06 12:21:42 +0900

    apikey 사용방법 변경

--- js/apikey.js
+++ js/apikey.js
@@ -1,7 +1,11 @@
 const config = {
-  covid_KEY: process.env.COVID_KEY,
-  covid_today_KEY: process.env.COVID_TODAY_KEY,
-  chart_KEY: process.env.CHART_KEY,
-  map_KEY: process.env.MAP_KEY,
-  kakao_appkey: process.env.KAKAO_APPKEY,
+  covid_KEY: 'oifWzVpNw53vJm1UYFkZrPDQcbKRnMqGC',
+  covid_today_KEY:
+    'YjEYZVzIxOyEXeagYbs7rNoExAOdQ9Kzgpn/a0FL3SPhkc2jUrX/TEc0XubMjZ4mvplbXIeHvgjZBjRxSU7Sag==',
+  chart_KEY:
+    'YjEYZVzIxOyEXeagYbs7rNoExAOdQ9Kzgpn/a0FL3SPhkc2jUrX/TEc0XubMjZ4mvplbXIeHvgjZBjRxSU7Sag==',
+  map_KEY:
+    'YjEYZVzIxOyEXeagYbs7rNoExAOdQ9Kzgpn%2Fa0FL3SPhkc2jUrX%2FTEc0XubMjZ4mvplbXIeHvgjZBjRxSU7Sag%3D%3D',
+  // 'YjEYZVzIxOyEXeagYbs7rNoExAOdQ9Kzgpn/a0FL3SPhkc2jUrX/TEc0XubMjZ4mvplbXIeHvgjZBjRxSU7Sag==',
+  appkey: '90f65cacde7d06a0fcba1f80975ef963',
 };
