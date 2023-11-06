7cb7ff64ccb7d834e5b4453e4779bc12756ed4c7
Jaeeun <jaeeun9862@gmail.com>
2023-11-06 11:50:47 +0900

    apikey 적용방법 수정

---
+++ js/apikey.js
@@ -0,0 +1,7 @@
+const config = {
+  covid_KEY: process.env.COVID_KEY,
+  covid_today_KEY: process.env.COVID_TODAY_KEY,
+  chart_KEY: process.env.CHART_KEY,
+  map_KEY: process.env.MAP_KEY,
+  kakao_appkey: process.env.KAKAO_APPKEY,
+};
